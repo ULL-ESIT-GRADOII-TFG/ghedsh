0f3d355ac1115dd6594d9ba33a48e22483e8e659
Carlos de Armas <alu0100816167@ull.edu.es>
2018-02-27 17:43:36 +0000

    Starting to refactor code

---
+++ lib/commands.rb
@@ -0,0 +1,28 @@
+
+
+#USER=1
+#ORGS=2
+#USER_REPO=10
+#ORGS_REPO=3
+#TEAM=4
+#ASSIG=6
+#TEAM_REPO=5
+
+class Commands
+  #def initialize(interface_object)
+    #@interface_data = interface_object
+  #end
+  
+  def orgs(params)
+    puts "HOLA"
+    puts "EL DEEP: #{@iterface_data.class}"
+    #puts "ORGS"
+    #case
+    #when params==USER
+      #puts "USUARIO"
+      #@sysbh.add_history_str(2,Organizations.new.show_orgs(@client,@config))
+    #when params==ORGS
+      #Organizations.new.show_orgs(@client,@config)
+    #end
+  end
+end
