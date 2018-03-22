58f3012b086318fd0bac1549ff79d0bfff551d13
Carlos de Armas <alu0100816167@ull.edu.es>
2018-03-22 22:42:11 +0000

    Refactor: rewriting cd command (early stage), each class will define it

--- lib/actions/user.rb
+++ lib/actions/user.rb
@@ -3,32 +3,68 @@ require 'octokit'
 require 'json'
 require 'require_all'
 require_rel '.'
+require 'tty-prompt'
 
 class User
   def info(client)
-    mem=client.user(client.login)
+    mem = client.user(client.login)
     puts m[:login]
     puts m[:name]
     puts m[:email]
   end
 
-  def self.shell_prompt(config, repo_path)
-    Rainbow(config['User'] + '> ').aqua
-    
-    #config['User'] + '> '
-      #if repo_path != ''
-        #config['User'] + '>' + "\e[31m#{config['Repo']}\e[0m" + '>' + repo_path.to_s + '> '
-      #else
-        #config['User'] + '>' + "\e[31m#{config['Repo']}\e[0m" + '> '
-      #end
+  def self.cd(type, name, enviroment)
+    case type
+    when 'org'
+      if name.class == Regexp
+        pattern = Regexp.new(name.source)
+        user_orgs = []
+        enviroment.client.organizations.each do |org|
+          user_orgs << org[:login] if pattern.match((org[:login]).to_s)
+        end
+        if user_orgs.empty?
+          puts "No repo match with #{name}"
+        else
+          prompt = TTY::Prompt.new
+          prompt.on(:keypress) do |event|
+            prompt.trigger(:keydown) if event.value == 'j'
+            prompt.trigger(:keyup) if event.value == 'k'
+          end
+          answer = prompt.select('Select desired repo', user_orgs)
+          if enviroment.client.organization_member?(answer.to_s, enviroment.client.login.to_s)
+            enviroment.config['Org'] = answer
+            enviroment.deep = Organization
+          end
+        end
+      else
+        if enviroment.client.organization_member?(name.to_s, enviroment.client.login.to_s)
+          enviroment.config['Org'] = name
+          enviroment.deep = Organization
+        else
+          puts "You are not currently #{name} member or #{name} is not an Organization."
+        end
+      end
+    else
+      puts 'nada'
+    end
+    enviroment
+  end
+
+  # Defined as method class in order to call it within context.rb
+  def self.shell_prompt(config, _repo_path)
+    if config['Repo'].nil?
+      Rainbow("#{config['User']}> ").aqua
+    else
+      Rainbow("#{config['User']}> ").aqua + Rainbow("#{config['Repo']}> ").color(236, 151, 21)
+    end
   end
 
   def open_user(client)
-    mem=client.user(client.login)
+    mem = client.user(client.login)
     Sys.new.open_url(mem[:html_url])
   end
 
-  def show_organizations(client, config)
+  def show_organizations(client, _config)
     puts
     organizations = client.list_organizations
     organizations.each do |i|
@@ -38,19 +74,33 @@ class User
 
   def show_commits(enviroment, params)
     options = {}
-    repo = enviroment.config["Repo"] || params[0]
-   
-    if params.empty?
-      options[:sha] = params[1]
+    if !enviroment.config['Repo'].nil?
+      repo = enviroment.config['Repo']
+      options[:sha] = if params.empty?
+                        'master'
+                      else
+                        params[0]
+                      end
     else
-      options[:sha] = "master"
+      repo = params[0]
+      options[:sha] = if params[1].nil?
+                        'master'
+                      else
+                        params[1]
+                      end
     end
-    mem=enviroment.client.commits("#{enviroment.config["User"]}/pagina-irene",options)
-    mem.each  do |i|
+
+    begin
+      mem = enviroment.client.commits("#{enviroment.config['User']}/#{repo}", options)
+      mem.each do |i|
         puts "\tSHA: #{i[:sha]}"
         puts "\t\t Commit date: #{i[:commit][:author][:date]}"
         puts "\t\t Commit author: #{i[:commit][:author][:name]}"
         puts "\t\t\t Commit message: #{i[:commit][:message]}"
+      end
+    rescue StandardError => exception
+      puts exception
+      puts "If you are not currently on a repo, USAGE TIP: `commits <repo_name> [branch_name]` (default: 'master')"
     end
   end
 end
