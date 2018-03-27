a85d264103e6bd8218df93320fedbf82333b7d44
Carlos de Armas <alu0100816167@ull.edu.es>
2018-03-27 18:34:09 +0100

    cd method supporting org scope and repo scope

--- lib/actions/user.rb
+++ lib/actions/user.rb
@@ -1,9 +1,7 @@
-require 'readline'
-require 'octokit'
-require 'json'
 require 'require_all'
 require_rel '.'
 require 'tty-prompt'
+require 'common'
 
 class User
   def info(client)
@@ -13,41 +11,76 @@ class User
     puts m[:email]
   end
 
-  def self.cd(type, name, enviroment)
-    case type
-    when 'org'
-      if name.class == Regexp
-        pattern = Regexp.new(name.source)
-        user_orgs = []
-        enviroment.client.organizations.each do |org|
-          user_orgs << org[:login] if pattern.match((org[:login]).to_s)
-        end
-        if user_orgs.empty?
-          puts "No repo match with #{name}"
-        else
-          prompt = TTY::Prompt.new
-          prompt.on(:keypress) do |event|
-            prompt.trigger(:keydown) if event.value == 'j'
-            prompt.trigger(:keyup) if event.value == 'k'
-          end
-          answer = prompt.select('Select desired repo', user_orgs)
-          if enviroment.client.organization_member?(answer.to_s, enviroment.client.login.to_s)
-            enviroment.config['Org'] = answer
-            enviroment.deep = Organization
-          end
-        end
+  def cd_org_scope(name, enviroment)
+    if name.class == Regexp
+      pattern = Regexp.new(name.source)
+      user_orgs = []
+      enviroment.client.organizations.each do |org|
+        user_orgs << org[:login] if pattern.match((org[:login]).to_s)
+      end
+      if user_orgs.empty?
+        puts "No organization match with #{name}"
       else
-        if enviroment.client.organization_member?(name.to_s, enviroment.client.login.to_s)
-          enviroment.config['Org'] = name
+        prompt = TTY::Prompt.new
+        prompt.on(:keypress) do |event|
+          prompt.trigger(:keydown) if event.value == 'j'
+          prompt.trigger(:keyup) if event.value == 'k'
+        end
+        answer = prompt.select('Select desired organization', user_orgs)
+        if enviroment.client.organization_member?(answer.to_s, enviroment.client.login.to_s)
+          enviroment.config['Org'] = answer
           enviroment.deep = Organization
-        else
-          puts "You are not currently #{name} member or #{name} is not an Organization."
         end
       end
     else
-      puts 'nada'
+      if enviroment.client.organization_member?(name.to_s, enviroment.client.login.to_s)
+        enviroment.config['Org'] = name
+        enviroment.deep = Organization
+      else
+        puts "You are not currently #{name} member or #{name} is not an Organization."
+      end
     end
     enviroment
+  end
+
+  def cd_repo_scope(name, enviroment)
+    if name.class == Regexp
+      pattern = Regexp.new(name.source)
+      user_repos = []
+      spinner = TTY::Spinner.new(Rainbow("Matching #{enviroment.client.login} repositories :spinner ...").color(4,255,0), format: :bouncing_ball)
+      spinner.auto_spin
+      enviroment.client.repositories.each do |repo|
+        user_repos << repo[:name] if pattern.match(repo[:name].to_s)
+      end
+      spinner.stop(Rainbow('done').color(4,255,0))
+
+      if user_repos.empty?
+        puts "No repository match with #{name}"
+      else
+        prompt = TTY::Prompt.new
+        prompt.on(:keypress) do |event|
+          prompt.trigger(:keydown) if event.value == 'j'
+          prompt.trigger(:keyup) if event.value == 'k'
+        end
+        answer = prompt.select('Select desired repository', user_repos)
+        enviroment.config['Repo'] = answer
+        enviroment.deep = User
+      end
+    else
+      if enviroment.client.repository?("#{enviroment.client.login}/#{name}")
+        puts 'seteo el repo'
+        enviroment.config['Repo'] = name
+        enviroment.deep = User
+      else
+        puts "Maybe #{name} is not a repository or currently does not exist."
+      end
+    end
+    enviroment
+  end
+
+  def cd(type, name, enviroment)
+    nav = {'org' => method(:cd_org_scope), 'repo' => method(:cd_repo_scope)}
+    nav[type].call(name, enviroment)
   end
 
   # Defined as method class in order to call it within context.rb
@@ -91,7 +124,7 @@ class User
     end
 
     begin
-      mem = enviroment.client.commits("#{enviroment.config['User']}/#{repo}", options)
+      mem = enviroment.client.commits("#{enviroment.client.login}/#{repo}", options)
       mem.each do |i|
         puts "\tSHA: #{i[:sha]}"
         puts "\t\t Commit date: #{i[:commit][:author][:date]}"
