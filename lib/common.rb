1a3682ff9253fab4e8745727f9a88694f239fc8b
Carlos de Armas <alu0100816167@ull.edu.es>
2018-04-03 17:35:21 +0100

    common file

--- lib/common.rb
+++ lib/common.rb
@@ -4,13 +4,18 @@ require 'actions/repo'
 require 'actions/system'
 require 'actions/teams'
 require 'actions/user'
-require 'rainbow'
+require 'rainbow' 
+require 'tty-spinner'
 
-USER = User
-ORG = Organization
-USER_REPO = 10
-ORGS_REPO = 3
-TEAM = Team
-ASSIG = Assignment
-TEAM_REPO = 5
+#USER = User
+#ORG = Organization
+#USER_REPO = 10
+#ORGS_REPO = 3
+#TEAM = Team
+#ASSIG = Assignment
+#TEAM_REPO = 5
 COMMANDS = {}
+User
+Organization
+Team
+Assignment
