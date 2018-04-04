edf89811c26118d5a179c31be77345ecddbaf021
Carlos de Armas <alu0100816167@ull.edu.es>
2018-04-04 13:03:48 +0100

    comments

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,6 +2,11 @@ require 'tty-prompt'
 require 'tty-spinner'
 require 'rainbow'
 
+# colors:
+  # error: .color('#cc0000')
+  # command not found: .yellow
+  # warning: .color('#9f6000')
+
 def custom_spinner(message)
-  spinner = TTY::Spinner.new(Rainbow("#{message}").color(4,255,0), format: :bouncing_ball)
+  spinner = TTY::Spinner.new(Rainbow("#{message}").color(79, 138, 16), format: :bouncing_ball)
 end
--- lib/interface.rb
+++ lib/interface.rb
@@ -53,7 +53,7 @@ class Interface
         else
           @shell_enviroment = ShellContext.new(options[:user], "#{ENV['HOME']}/.ghedsh", options[:token])
         end
-        spinner.stop(Rainbow('done').color(4, 255, 0))
+        spinner.stop(Rainbow('done').color(79, 138, 16))
         run
       rescue SystemExit, Interrupt
         raise
