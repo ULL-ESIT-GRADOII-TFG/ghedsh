f159be3c5f5a5d4083efb124c2f338ffddbdd39f
Carlos de Armas <alu0100816167@ull.edu.es>
2018-04-07 19:53:25 +0100

    adding perform_git_commit method

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,16 +1,17 @@
 require 'tty-prompt'
 require 'tty-spinner'
 require 'rainbow'
+require 'fileutils'
 
 # colors:
-  # error: .color('#cc0000')
-  # command not found: .yellow
-  # warning: .color('#9f6000')
-  # info: .color(#00529B)
-  # success: .color(79, 138, 16)
+# error: .color('#cc0000')
+# command not found: .yellow
+# warning: .color('#9f6000')
+# info: .color(#00529B)
+# success: .color(79, 138, 16)
 
 def custom_spinner(message)
-  spinner = TTY::Spinner.new(Rainbow("#{message}").color(79, 138, 16), format: :bouncing_ball)
+  spinner = TTY::Spinner.new(Rainbow(message.to_s).color(79, 138, 16), format: :bouncing_ball)
 end
 
 def show_matching_items(item, pattern)
@@ -32,24 +33,36 @@ end
 def repo_creation_guide
   puts Rainbow("Select 'Default' to create a quick public repo.").color('#f18973')
   puts Rainbow("Select 'Custom' for private/public repo whith specific options.").color('#f18973')
-  puts Rainbow("To skip any option just hit Enter (Default options).").color('#f18973')
+  puts Rainbow('To skip any option just hit Enter (Default options).').color('#f18973')
   puts
-  choices = %w{Default Custom}
+  choices = %w[Default Custom]
   prompt = TTY::Prompt.new
   answer = prompt.select('Select configuration', choices)
   if answer == 'Default'
     return answer
   else
-    puts Rainbow("Answer questions with yes/true or no/false").color('#f18973')
+    puts Rainbow('Answer questions with yes/true or no/false').color('#f18973')
     custom_options = prompt.collect do
       key(:private).ask('(Private repo? (Default: false) [yes/true, no/false]', convert: :bool)
       key(:description).ask('Write description of the repo')
       key(:has_issues).ask('Has issues? (Default:false) [yes/true, no/false]', convert: :bool)
       key(:has_wiki).ask('Has wiki? (Default: true) [yes/true, no/false]', convert: :bool)
-      key(:auto_init).ask('Create an initial commit with empty README? (Default: false) (if you want .gitignore template must be yes/true)', 
+      key(:auto_init).ask('Create an initial commit with empty README? (Default: false) (if you want .gitignore template must be yes/true)',
                           convert: :bool)
       key(:gitignore_template).ask('Desired language or platform for .gitignore template')
     end
     return custom_options.compact!
   end
+end
+
+def perform_git_clone(https_url)
+  FileUtils.mkdir_p("#{Dir.home}/ghedsh_cloned")
+  FileUtils.cd("#{Dir.home}/ghedsh_cloned") do
+    https_url.each do |i|
+      system("git clone --progress #{i}")
+    end
+  end
+rescue StandardError => exception
+  puts Rainbow(exception.message.to_s).color('#cc0000')
+  puts
 end
