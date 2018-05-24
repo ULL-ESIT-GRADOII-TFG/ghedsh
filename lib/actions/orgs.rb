6bad4d6d020dba8bf33b2221887d30086b6b31a9
Carlos de Armas <alu0100816167@ull.edu.es>
2018-05-24 18:46:21 +0100

    Fixes #45: set CWD to default directory

--- lib/actions/orgs.rb
+++ lib/actions/orgs.rb
@@ -179,9 +179,9 @@ class Organization
     unless ssh_url.empty?
       perform_git_clone(ssh_url, custom_path)
       if custom_path.nil?
-        puts Rainbow("Cloned files are on directory #{Dir.home}/ghedsh_cloned").color(INFO_CODE)
+        puts Rainbow("Cloned into #{Dir.pwd}").color(INFO_CODE).underline
       else
-        puts Rainbow("Cloned files are on directory #{Dir.home}#{custom_path}").color(INFO_CODE)
+        puts Rainbow("Cloned into #{Dir.home}#{custom_path}").color(INFO_CODE).underline
       end
       puts
     end
